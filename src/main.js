f3a10df176e63215746f5cca3bd434642874e930
Kristina Andersen <kris91r3@stud.kea.dk>
2022-09-13 14:27:48 +0200

    ny html

--- src/main.js
+++ src/main.js
@@ -1,3 +1,27 @@
 import "./style.css";
 /* Import af Motion One bibliotek */
 import { animate, stagger, inView, scroll, timeline } from "motion";
+
+/*Animation på desktop og op */
+if (window.innerWidth > 760) {
+  inView(".textinview", ({ target }) => {
+    animate(target, { translateX: [-600, 0] }, { duration: 1 });
+    return () => {
+      animate(target, { translateX: [-600, 0] }, { duration: 1 });
+    };
+  });
+
+  inView(".billedeview", ({ target }) => {
+    animate(target, { translateX: [500, 0] }, { duration: 1 });
+    return () => {
+      animate(target, { translateX: [0, -500] }, { duration: 1 });
+    };
+  });
+}
+
+/* animationer på mobil */
+if (window.innerWidth <= 760) {
+}
+
+// røde bjæke i toppen
+scroll(animate(".progress-bar", { scaleX: [0, 1] }));
